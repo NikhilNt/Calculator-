e070bfd6d10f15fa0211c351728b969e89b59bb3
Nikhil Tanappagol <nikhiltanappagol@Nikhils-MacBook-Pro.local>
2017-06-20 15:41:53 -0500

    Initial Commit

---
+++ miniCraftCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miniCraftCal
+//
+//  Created by Nikhil Tanappagol on 6/20/17.
+//  Copyright © 2017 Nikhil Tanappagol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
